5f140f2de4259d04cb05bdf825b93f3c751926a7
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2026-05-21 23:39:51 +0800

    fix: allow logger thread to gracefully quit

--- captcha_bot/__main__.py
+++ captcha_bot/__main__.py
@@ -1,6 +1,9 @@
 import uvloop
 
+from captcha_bot.custom_logger import getLogger
 from captcha_bot.main import main
 
 if __name__ == "__main__":
     uvloop.run(main())
+    getLogger().queue.put(None)
+    getLogger().thread.join()
